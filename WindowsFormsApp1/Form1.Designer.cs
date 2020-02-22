2ed610afe53279d9c0060d63893df69bccdae454
huchao <2502273839@qq.com>
2020-02-22 14:23:09 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.operation = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.result_num = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // num1
+            // 
+            this.num1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.num1.Location = new System.Drawing.Point(156, 139);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(100, 28);
+            this.num1.TabIndex = 3;
+            this.num1.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(518, 139);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(100, 28);
+            this.num2.TabIndex = 2;
+            // 
+            // operation
+            // 
+            this.operation.FormattingEnabled = true;
+            this.operation.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.operation.Location = new System.Drawing.Point(156, 247);
+            this.operation.Name = "operation";
+            this.operation.Size = new System.Drawing.Size(121, 26);
+            this.operation.TabIndex = 4;
+            this.operation.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "第一个数：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(520, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "第二个数：";
+            // 
+            // result_num
+            // 
+            this.result_num.AutoSize = true;
+            this.result_num.Location = new System.Drawing.Point(515, 255);
+            this.result_num.Name = "result_num";
+            this.result_num.Size = new System.Drawing.Size(62, 18);
+            this.result_num.TabIndex = 7;
+            this.result_num.Text = "result";
+            this.result_num.Click += new System.EventHandler(this.result_num_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 429);
+            this.Controls.Add(this.result_num);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.operation);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.ComboBox operation;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.Label result_num;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int result = 0;
+                int first_num = Convert.ToInt32(num1.Text);
+                int second_num = Convert.ToInt32(num2.Text);
+                String operations = operation.SelectedItem.ToString();
+                switch (operations)
+                {
+                    case "+":
+                        result = first_num + second_num;
+                        break;
+                    case "-":
+                        result = first_num - second_num;
+                        break;
+                    case "*":
+                        result = first_num * second_num;
+                        break;
+                    case "/":
+                        while(second_num==0)
+                        {
+                            Console.WriteLine("请输入一个不为零的整数：\n");
+                            second_num = Convert.ToInt32(Console.ReadLine());
+                        }
+                        result = first_num / second_num;
+                        break;
+                }
+                result_num.Text = result.ToString();
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("格式错误！");
+            }catch(OverflowException)
+            {
+                Console.WriteLine("溢出！");
+            }catch (Exception)
+            {
+                Console.WriteLine("错误！");
+            }
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void result_num_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
