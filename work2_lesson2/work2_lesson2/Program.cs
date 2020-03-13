7f4981e31cc1ba57c5d8e86bb5ce10dfd83eda40
HuChao <2502273839@qq.com>
2020-03-13 16:57:28 +0800

    第四次作业提交

---
+++ work2_lesson2/work2_lesson2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work2_lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] res = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("数组的最大值为：" + Max(res));
+            Console.WriteLine("数组的最小值为：" + Min(res));
+            Console.WriteLine("数组的平均值为：" + Average(res));
+            Console.WriteLine("数组的和为：" + Sum(res));
+            Console.ReadKey();
+        }
+        static int Max(int [] a)
+        {
+            int max = a[0];
+            foreach(int num in a)
+            {
+                if (max < num)
+                    max = num;
+            }
+            return max;
+        }
+
+        static int Min(int [] a)
+        {
+            int min = a[0];
+            foreach(int num in a)
+            {
+                if (min > num)
+                    min = num;
+            }
+            return min;
+        }
+
+        static int Average(int []a)
+        {
+            int average=0, length=0;
+            foreach(int num in a)
+            {
+                length++;
+                average += num;
+            }
+            average /= length;
+            return average;
+        }
+
+        static int Sum(int[] a)
+        {
+            int sum = 0;
+            foreach (int num in a)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
---
+++ work3_lesson2/work3_lesson2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work3_lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = EstablishArray(2, 200);
+            GetResults(array);
+            foreach(int num in array)
+            {
+                if (num != -1)
+                    Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+
+        static int[] EstablishArray(int fore,int end)
+        {
+            int length = end - fore+1;
+            int[] array = new int[length];
+            for(int i=0;i<length;i++)
+            {
+                array[i] = fore+i;
+            }
+            return array;
+        }
+
+        static void GetResults(int []array)
+        {
+            int border = (int)Math.Sqrt(array.Max());
+            for(int i=2;i<border;i++)
+            {
+                for(int j=0;j<array.Length;j++)
+                {
+                    if(array[j]!=-1&&array[j]%i==0&&i<array[j])
+                    {
+                        array[j] = -1;
+                    }
+                }
+            }
+
+        }
+
+
+    }
+}
